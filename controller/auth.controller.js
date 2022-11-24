79ceee3bdcd752b1a9832290aeeef278d558fb0a
NaoMauss <91453617+NaoMauss@users.noreply.github.com>
2022-11-24 18:28:02 +0100

    first commit

---
+++ controller/auth.controller.js
@@ -0,0 +1,36 @@
+const UserModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.TOKEN_SECRET, {
+        expiresIn: 3 * 24 * 60 * 60 * 1000
+    })
+}
+
+module.exports.signUp = async (req, res) => {
+    const {pseudo, email, password} = req.body
+
+    try{
+        const user = await UserModel.create({pseudo, email, password});
+        res.status(201).json({ user: user._id});
+    }
+    catch(err){
+        res.status(200).send({err})
+    }
+}
+
+module.exports.signIn = async (req, res) => {
+    const { email, password} = req.body
+
+    try{
+        const user = await UserModel.login(email, password);
+        const token = createToken(user._id)
+
+    } catch(err){
+
+    }
+}
+
+module.exports.logout = (req,res) => {
+
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const userRoutes = require('./routes/user.routes')
+
+require('dotenv').config({path: './config/.env'})
+require('./config/db.js')
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+//routes
+app.use('/api/user', userRoutes);
+
+
+//server
+app.listen(process.env.PORT , () => {
+    console.log(`listening on port ${process.env.PORT}`)
+})
