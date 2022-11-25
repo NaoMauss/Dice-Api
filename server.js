4c4e58ff80f94df23e2647dcb5e31ceac698a85e
NaoMauss <91453617+NaoMauss@users.noreply.github.com>
2022-11-26 00:54:00 +0100

    register and login

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const express = require('express')
-const userRoutes = require('./routes/user.routes')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user.routes");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db.js");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+const app = express();
 
-require('dotenv').config({path: './config/.env'})
-require('./config/db.js')
-const app = express()
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieParser());
+
+//jwt
+app.get("*", checkUser); // s'execute à chaque redirection pour vérifier le cookie
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
 
 //routes
-app.use('/api/user', userRoutes);
-
+app.use("/api/user", userRoutes);
 
 //server
-app.listen(process.env.PORT , () => {
-    console.log(`listening on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
