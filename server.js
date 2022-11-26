740cf6c15cd589b5dda60aaec5d75eaa6e06e591
NaoMauss <91453617+NaoMauss@users.noreply.github.com>
2022-11-26 15:45:28 +0100

    Added link controller

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
+const linkRoutes = require("./routes/link.routes");
+const LinkModel = require("./models/links.model");
+const linkController = require("./controller/link.controller");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db.js");
 const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const cors = require("cors");
+const cron = require("node-cron");
 
 const app = express();
 
@@ -25,6 +29,9 @@ app.get("/jwtid", requireAuth, (req, res) => {
 
 //routes
 app.use("/api/user", userRoutes);
+app.use("/api/link", linkRoutes);
+
+app.get("/:short_url", linkController.redirectToLink);
 
 //server
 app.listen(process.env.PORT, () => {
